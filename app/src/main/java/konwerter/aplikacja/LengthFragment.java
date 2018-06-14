f982f6694f8aeb9db40dc22b9d0746021ef93891
Jakub <Jakub@JAKUB-PC>
2018-06-14 03:48:57 +0200

    Initial commit

---
+++ app/src/main/java/konwerter/aplikacja/LengthFragment.java
@@ -0,0 +1,28 @@
+package konwerter.aplikacja;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LengthFragment extends Fragment {
+
+    public LengthFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_length, container, false);
+    }
+
+}
---
+++ app/src/main/java/konwerter/aplikacja/MassFragment.java
@@ -0,0 +1,27 @@
+package konwerter.aplikacja;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MassFragment extends Fragment {
+
+    public MassFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mass, container, false);
+    }
+
+}
